38aca752124d8c17336f2c49887a5338baa080c6
optical-lift <gleamingbrook@gmail.com>
2026-08-30 13:24:27 -0500

    Load WNPH reader stylesheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import './globals.css';
+import './reader.css';
 
 export const metadata: Metadata = {
   title: 'Write Now Publishing House',
